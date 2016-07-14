e361e45bf58e3d825d1a060ab014b1aac22e9855
Ryo Wheatley <ryo.capricorn92@gmail.com>
2016-07-14 18:55:16 -0400

    added a commented React template

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+// ReactDOM.render(<Router history={browserHistory}>
+//     <Route path="/" component={App}>
+//       <Route path="about" component={About}/>
+//       <Route path="users" component={Users}>
+//         <Route path="/user/:userId" component={User}/>
+//       </Route>
+//       <Route path="*" component={NoMatch}/>
+//     </Route>
+//   </Router>, document.getElementById('app'))
