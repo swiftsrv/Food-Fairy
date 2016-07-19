dcd48f2021e180ca9f24ad8e51adff4354306766
FELIPE MATSUMOTO <felipetmatsumoto@gmail.com>
2016-07-19 15:06:53 -0300

    change server.js

--- server/server.js
+++ server/server.js
@@ -8,9 +8,10 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client'));
-app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/node_modules', express.static(__dirname + '/../node_modules'));
 
 app.get('/', function (req, res) {
+  res.sendFile('index-homepage.html')
   res.status(200);
 });
 
