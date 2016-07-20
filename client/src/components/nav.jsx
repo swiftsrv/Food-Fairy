652d4f1561b2139d9893985f37b83c2d6d71f30d
Ryo Wheatley <ryo.capricorn92@gmail.com>
2016-07-20 15:10:21 -0400

    reworking nav

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -1,6 +1,12 @@
 var Nav = (props) => (
   <nav className="navbar">
-    <Navitem />
+    <ul>
+      <a href='/home'><li>Home</li></a>
+      <a href='/search'><li>Search</li></a>
+      <li><div className="logoimage"></div></li>
+      <a href='/saved'><li>Saved</li></a>
+      <a href='/login'><li>Login</li></a>
+    </ul>
   </nav>
 )
 
