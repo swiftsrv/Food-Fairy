5f172eec1470ffb06dcd9423cd84fd1a78e740ec
Ryo Wheatley <ryo.capricorn92@gmail.com>
2016-07-20 16:45:28 -0400

    reworking nav w bootstrap

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -1,12 +1,12 @@
 var Nav = (props) => (
-  <nav className="container-fluid navbar">
-    <ul>
-      <a href='/home'><li>Home</li></a>
-      <a href='/search'><li>Search</li></a>
-      <li><div className="logoimage"></div></li>
-      <a href='/saved'><li>Saved</li></a>
-      <a href='/login'><li>Login</li></a>
-    </ul>
+  <nav className="container-fluid">
+    <div className="row text-center navbar">
+      <a href="/home"><div className="col-md-2 col-sm-2 navpad">Home</div></a>
+      <a href="/search"><div className="col-md-2 col-sm-2 navpad line">Search</div></a>
+      <div className="col-md-4 col-sm-4"><img className="center-block logo" src="imgs/logo2.png" /></div>
+      <a href="/saved"><div className="col-md-2 col-sm-2 navpad line">Saved</div></a>
+      <a href="/login"><div className="col-md-2 col-sm-2 navpad">Login</div></a>
+    </div>
   </nav>
 )
 
