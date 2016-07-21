8806688dfb15c61e4d2220d51c12b66b7648504d
FELIPE MATSUMOTO <felipetmatsumoto@gmail.com>
2016-07-21 13:58:41 -0300

    change API key to a different file

--- index-server.js
+++ index-server.js
@@ -2,8 +2,6 @@ var app = require('./server/server.js');
 
 var port = process.env.PORT || 3000;
 
-var SPOONTACULAR_API_KEY = SPOONTACULAR_API_KEY || process.env.SPOONTACULAR_API_KEY;
-
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
