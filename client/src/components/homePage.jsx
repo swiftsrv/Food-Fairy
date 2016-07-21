fa8da9aa6bef9119b0712bf5c078834d3366efef
Ryo Wheatley <ryo.capricorn92@gmail.com>
2016-07-21 14:27:11 -0400

    working on home divs

--- client/src/components/homePage.jsx
+++ client/src/components/homePage.jsx
@@ -4,8 +4,9 @@ class Home extends React.Component {
   }
   render(){
     return(
-      <div className='home'>
-        <Header />
+      <Header />
+      <div className="container-fluid"><img className="center-block homebg" src="imgs/bg.png"/></div>
+      <div className="homebox">
         <SearchBox searchAPI={this.props.searchAPI}/>
       </div>
     )
