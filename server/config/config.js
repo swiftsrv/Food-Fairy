16122ac940445d3f4e939dad006cb40d79f77df1
Jason Bland <jtbland@gmail.com>
2016-07-22 13:37:11 -0700

    (feat) Configure passport to use Auth0 strategy (feat) Add secrets to local .env for 'heroku local'

---
+++ server/config/config.js
@@ -0,0 +1,10 @@
+// Spoontactular API key goes here
+window.SPOONTACULAR_API_KEY = 'R6un6vpSqfmshTqGWty9ffZySRO0p1QAKU4jsn5Zy79FEs8QMm';
+
+//Signed up for education license which expires on 9/18/2016
+
+//Website for API hosting and documentation
+//https://market.mashape.com/spoonacular/recipe-food-nutrition#find-by-ingredients
+
+//Food API and Recipe API home page
+//https://spoonacular.com/food-api
