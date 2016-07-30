69e05d5b6bf1289763f6ac51da9819bb08cd2940
jmg2107 <jenjengoo@gmail.com>
2016-07-29 23:23:02 -0700

    (Feature) Change all the logos to our company name

--- client/src/components/homePage.jsx
+++ client/src/components/homePage.jsx
@@ -11,7 +11,7 @@ var Home = (props) => (
     {/* green welcome div at the bottom of the home page */}
     <div className="homebox">
       <div className="homeIntro center-block">
-        <p>Welcome to FoodFairy.com! Just tell the Fairy what ingrediants you have lying around your house and she'll whip you up a recipe in no time!</p>
+        <p>Welcome to Foraje.com! Just tell us what ingrediants you have lying around your house and we'll whip you up a recipe in no time!</p>
       </div>
       <SearchBox searchAPI={props.searchAPI}/>
     </div>
